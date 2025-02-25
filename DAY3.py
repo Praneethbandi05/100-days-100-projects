1789ccb954e608fe9ace08ccf36a79122fe6d4a0
Praneethbandi05 <praneethb523@gmail.com>
2025-02-25 12:32:54 +0530

    DAY3.py

---
+++ DAY3.py
@@ -0,0 +1,19 @@
+print(" WELCOME TO TEASURE ISLAND........")
+choice1 = input(" You're moving forward select left or right\n").lower()
+if choice1=="left":
+       choice2 = input("CONGRATS,,NOW YOU ARE IN A ISLAND,CHOOSE ONE [wait] OR [swim]\n").lower()
+       if choice2 == "wait":
+           choice3 = input("CHOOSE ONE DOOR TO GET TEASURE [red]  [yellow]  [blue]\n").lower()
+           if choice3 == "red":
+               print("THERE IS FULL OF SNACKS  Game over")
+           elif choice3 == "yellow":
+               print("!!!!!HURRY YOU FIND THE TEASURE YOU WON!!!")
+           elif choice3 == "blue":
+               print("THE DOOR IS  FULL OF FIRE   Game over!!!!")
+           else:
+               print("invalid input GAME OVER !!!!")
+
+       else:
+           print(" THERE IS A MONSTER IN THE WATER......GAME OVER!!!")
+else:
+       print("WRONG STEP GAME OVER!!!")
